9cc69a83e00d80105852c7873b888a88fe8fadd4
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-04-24 19:08:53 +0700

    fix: make routing basename environment-aware for local dev

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/garingalami-fe/">
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/garingalami-fe/'}>
       <App />
     </BrowserRouter>
   </StrictMode>,
